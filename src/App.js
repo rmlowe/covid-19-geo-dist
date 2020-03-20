1313db094b6f4d0e9cfbf685eb02df731c3dd3f3
Robert Lowe <robertmlowe@rmlowe.com>
2020-03-21 00:56:45 +0800

    Extracting components for date picker and table

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import parse from 'csv-parse';
 
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import { DateRange } from 'react-date-range';
+import DatePicker from './DatePicker';
+import CountrySummary from './CountrySummary';
+
+const minDate = data => new Date(Math.min(...data.map(record => record.date)));
+
+const maxDate = data => new Date(Math.max(...data.map(record => record.date)));
 
 class App extends React.Component {
-  minDate = data => new Date(Math.min(...data.map(record => record.date)));
-
-  maxDate = data => new Date(Math.max(...data.map(record => record.date)));
-
   async componentDidMount() {
     const response = await axios.get('/geodist.csv');
     parse(response.data, (err, output) => {
@@ -24,13 +23,11 @@ class App extends React.Component {
         };
       });
       this.setState({
-        datePickerVisible: false,
         dateRange: {
-          startDate: this.minDate(data),
-          endDate: this.maxDate(data)
+          startDate: minDate(data),
+          endDate: maxDate(data)
         },
-        data,
-        sortedBy: 1
+        data
       });
     });
   }
@@ -47,157 +44,6 @@ class App extends React.Component {
     }, {});
   }
 
-  flags = {
-    CN: '🇨🇳',
-    IT: '🇮🇹',
-    IR: '🇮🇷',
-    ES: '🇪🇸',
-    US: '🇺🇸',
-    FR: '🇫🇷',
-    KR: '🇰🇷',
-    DE: '🇩🇪',
-    CH: '🇨🇭',
-    UK: '🇬🇧',
-    NL: '🇳🇱',
-    AT: '🇦🇹',
-    BE: '🇧🇪',
-    NO: '🇳🇴',
-    SE: '🇸🇪',
-    DK: '🇩🇰',
-    JP: '🇯🇵',
-    MY: '🇲🇾',
-    JPG11668: '🚢',
-    CA: '🇨🇦',
-    PT: '🇵🇹',
-    AU: '🇦🇺',
-    CZ: '🇨🇿',
-    QA: '🇶🇦',
-    IL: '🇮🇱',
-    BR: '🇧🇷',
-    EL: '🇬🇷',
-    FI: '🇫🇮',
-    IE: '🇮🇪',
-    SG: '🇸🇬',
-    PK: '🇵🇰',
-    PL: '🇵🇱',
-    SI: '🇸🇮',
-    RO: '🇷🇴',
-    EE: '🇪🇪',
-    BH: '🇧🇭',
-    IS: '🇮🇸',
-    CL: '🇨🇱',
-    LU: '🇱🇺',
-    PH: '🇵🇭',
-    EG: '🇪🇬',
-    TR: '🇹🇷',
-    TH: '🇹🇭',
-    ID: '🇮🇩',
-    SA: '🇸🇦',
-    EC: '🇪🇨',
-    IN: '🇮🇳',
-    IQ: '🇮🇶',
-    RU: '🇷🇺',
-    PE: '🇵🇪',
-    KW: '🇰🇼',
-    LB: '🇱🇧',
-    MX: '🇲🇽',
-    ZA: '🇿🇦',
-    AM: '🇦🇲',
-    AE: '🇦🇪',
-    PA: '🇵🇦',
-    SM: '🇸🇲',
-    TW: '🇹🇼',
-    SK: '🇸🇰',
-    CO: '🇨🇴',
-    AR: '🇦🇷',
-    RS: '🇷🇸',
-    BG: '🇧🇬',
-    HR: '🇭🇷',
-    UY: '🇺🇾',
-    VN: '🇻🇳',
-    DZ: '🇩🇿',
-    HU: '🇭🇺',
-    LV: '🇱🇻',
-    CR: '🇨🇷',
-    BN: '🇧🇳',
-    AL: '🇦🇱',
-    CY: '🇨🇾',
-    MA: '🇲🇦',
-    AD: '🇦🇩',
-    JO: '🇯🇴',
-    MT: '🇲🇹',
-    BY: '🇧🇾',
-    PS: '🇵🇸',
-    MK: '🇲🇰',
-    LK: '🇱🇰',
-    OM: '🇴🇲',
-    KZ: '🇰🇿',
-    BA: '🇧🇦',
-    MD: '🇲🇩',
-    SN: '🇸🇳',
-    AZ: '🇦🇿',
-    GE: '🇬🇪',
-    LT: '🇱🇹',
-    VE: '🇻🇪',
-    TN: '🇹🇳',
-    NZ: '🇳🇿',
-    BF: '🇧🇫',
-    LI: '🇱🇮',
-    KH: '🇰🇭',
-    UZ: '🇺🇿',
-    AF: '🇦🇫',
-    DO: '🇩🇴',
-    XK: '🇽🇰',
-    UA: '🇺🇦',
-    JM: '🇯🇲',
-    CD: '🇨🇩',
-    MV: '🇲🇻',
-    BO: '🇧🇴',
-    HN: '🇭🇳',
-    CU: '🇨🇺',
-    PY: '🇵🇾',
-    RW: '🇷🇼',
-    BD: '🇧🇩',
-    CM: '🇨🇲',
-    MC: '🇲🇨',
-    TT: '🇹🇹',
-    GT: '🇬🇹',
-    ME: '🇲🇪',
-    NG: '🇳🇬',
-    GH: '🇬🇭',
-    KE: '🇰🇪',
-    CI: '🇨🇮',
-    ET: '🇪🇹',
-    SC: '🇸🇨',
-    GY: '🇬🇾',
-    MN: '🇲🇳'
-  };
-
-  numericColumn = (name, propKey) => ({
-    name,
-    formatter: (key, value) => new Intl.NumberFormat().format(value[propKey]),
-    alignRight: true,
-    comparator: (a, b) => b[propKey] - a[propKey],
-    sortDescending: true
-  });
-
-  columns = [
-    {
-      name: 'Country',
-      formatter: (key, value) => {
-        const flag = this.flags[key];
-        return flag ? flag + ' ' + value.countryName : (key + ' ' + value.countryName)
-      },
-      alignRight: false,
-      comparator: (a, b) => a.countryName.localeCompare(b.countryName),
-      sortDescending: false
-    },
-    this.numericColumn('New', 'newCases'),
-    this.numericColumn('Deaths', 'deaths')
-  ]
-
-  columnClassName = column => column.alignRight ? 'text-right' : 'text-left';
-
   renderContent() {
     if (this.state) {
       const byCountryCode = this.byCountryCode(this.state.data.filter(record =>
@@ -205,48 +51,13 @@ class App extends React.Component {
 
       return (
         <div>
-          <div className="row justify-content-between">
-            <div className="col-auto">{this.state.dateRange.startDate.toLocaleDateString()} &ndash; {this.state.dateRange.endDate.toLocaleDateString()}</div>
-            <div className="col-auto">
-              <button className="btn btn-primary py-0" onClick={() => this.setState({ datePickerVisible: !this.state.datePickerVisible })}>
-                {this.state.datePickerVisible ? 'Hide date picker' : 'Show date picker'}
-              </button>
-            </div>
-          </div>
-          {this.state.datePickerVisible && <DateRange
-            onChange={item => this.setState({ dateRange: item.range1 })}
-            ranges={[this.state.dateRange]}
-            minDate={this.minDate(this.state.data)}
-            maxDate={this.maxDate(this.state.data)}
-          />}
-          <table className="table">
-            <thead>
-              <tr>{this.columns.map((column, index) =>
-                <th className={this.columnClassName(column)} key={index}>{
-                  index === this.state.sortedBy ?
-                    <button className="btn btn-link m-0 p-0 text-nowrap">
-                      <b>{column.name}</b> <i className={'fas ' + (column.sortDescending ? 'fa-sort-down' : 'fa-sort-up')}></i>
-                    </button> :
-                    <button
-                      className="btn btn-link m-0 p-0"
-                      onClick={event => this.setState({ sortedBy: index })}>
-                      {column.name}
-                    </button>
-                }</th>
-              )}</tr>
-            </thead>
-            <tbody>
-              {Object.entries(byCountryCode)
-                .sort(([key1, value1], [key2, value2]) => this.columns[this.state.sortedBy].comparator(value1, value2))
-                .map(([key, value]) => (
-                  <tr key={key}>
-                    {this.columns.map((column, index) => (
-                      <td className={this.columnClassName(column)} key={index}>{column.formatter(key, value)}</td>
-                    ))}
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+          <DatePicker
+            dateRange={this.state.dateRange}
+            onChange={dateRange => this.setState({ dateRange })}
+            minDate={minDate(this.state.data)}
+            maxDate={maxDate(this.state.data)}
+          />
+          <CountrySummary byCountryCode={byCountryCode} />
         </div>
       );
     } else {
