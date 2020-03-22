e6ed0f83ff96bf32c5a6a7629a75b95b36677caf
Robert Lowe <robertmlowe@rmlowe.com>
2020-03-22 18:53:50 +0800

    Time series chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import parse from 'csv-parse';
+import { ResponsiveContainer, AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Area } from 'recharts';
 
 import DatePicker from './DatePicker';
 import Total from './Total';
@@ -11,6 +12,9 @@ const minDate = data => new Date(Math.min(...data.map(record => record.date)));
 const maxDate = data => new Date(Math.max(...data.map(record => record.date)));
 
 const reducer = (acc, cur) => ({
+  date: acc.date,
+  dateString: acc.dateString,
+  dateNumber: acc.dateNumber,
   countryName: acc.countryName,
   newCases: acc.newCases + cur.newCases,
   deaths: acc.deaths + cur.deaths
@@ -27,8 +31,11 @@ class App extends React.Component {
     const response = await axios.get('/geodist.csv');
     parse(response.data, (err, output) => {
       const data = output.slice(1).map(record => {
+        const date = new Date(record[3], record[2] - 1, record[1]);
         return {
-          date: new Date(record[3], record[2] - 1, record[1]),
+          date,
+          dateString: date.toLocaleDateString(),
+          dateNumber: date.getTime(),
           countryName: record[6].replace(/_/g, ' '),
           countryCode: record[7],
           newCases: +record[4],
@@ -51,6 +58,9 @@ class App extends React.Component {
         record.date >= this.state.dateRange.startDate && record.date <= this.state.dateRange.endDate);
       const totals = filtered.reduce(reducer);
 
+      const byDate = Object.values(reduceByKey(filtered, 'date', reducer));
+      byDate.sort((a, b) => a.dateNumber - b.dateNumber);
+
       return (
         <div>
           <DatePicker
@@ -60,13 +70,35 @@ class App extends React.Component {
             maxDate={maxDate(this.state.data)}
           />
           <div className="row justify-content-around py-1 border-top">
-            <div className="col-4">
+            <div className="col-auto">
               <Total label="Total cases" value={totals.newCases} />
             </div>
-            <div className="col-4">
+            <div className="col-auto">
               <Total label="Total deaths" value={totals.deaths} />
             </div>
           </div>
+          <ResponsiveContainer width="100%" height={150}>
+            <AreaChart data={byDate} margin={{ top: 20 }}>
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="dateString" hide={true} />
+              {/* <XAxis dataKey="dateNumber" type="number" scale="time" domain={['auto', 'auto']} tickFormatter={tick => new Date(tick).toLocaleDateString()} /> */}
+              <YAxis />
+              <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+              <Tooltip />
+              <Legend />
+              <Area name="New confirmed cases" type="monotone" dataKey="newCases" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+              <Area name="Deaths" type="monotone" dataKey="deaths" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            </AreaChart>
+          </ResponsiveContainer>
           <CountrySummary byCountryCode={reduceByKey(filtered, 'countryCode', reducer)} />
         </div >
       );
--- src/Total.js
+++ src/Total.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Total = props => (
   <div>
-    <span className="text-muted">{props.label}</span>
-    <br />
-    <strong style={{ fontSize: '150%' }}>{new Intl.NumberFormat().format(props.value)}</strong>
+    <span className="text-muted">{props.label}:</span> <strong style={{ fontSize: '150%' }}>{new Intl.NumberFormat().format(props.value)}</strong>
   </div>
 );
 
